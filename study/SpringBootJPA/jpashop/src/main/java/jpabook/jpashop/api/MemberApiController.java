1be9e7934dfde51ba2f3a0d1ba862b3e9f273c28
titanfall <qkrwogh7366@gmail.com>
2026-08-08 18:47:45 +0900

    feat: 회원 API 개발 (등록/수정/조회 V1, V2)

---
+++ study/SpringBootJPA/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,105 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController // @Controller, @ResponseBody
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> memberV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+    
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName()); // DTO > api는 영향을 받지 않게됨(컴파일 타임에 감지 가능)
+        // member.name > member.username
+        // membser.setName() > member.setUsername()
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id); // jpa 영속상태가 끊기는걸 방지할수있다.
+        return new UpdateMemberResponse(findMember.getName(), findMember.getId());
+    }
+
+    // 업데이트용 DTO
+    @Data
+    static class UpdateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private String name;
+        private Long id;
+    }
+
+    // 별도의 DTO
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty // 필요한 시점에서만 null 검사가 가능해진다.
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
--- study/SpringBootJPA/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ study/SpringBootJPA/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,10 +20,14 @@ public class Member {
     // if) 필요에 의해 엔티티에 password를 추가하였다.
     // 이때문에 1. 패스워드 노출, 2. api 스펙 변경 두가지 오류가 발생한다.
     // private String password;
+
+    // @NotEmpty // 요구되는 스펙이 다를경우 문제가 됨 (null 허용 및 미허용)
     private String name;
+
     @Embedded
     private Address address;
 
+    @JsonIgnore // json 변환에서 제외해줌
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
